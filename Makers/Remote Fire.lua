5b9b55ed1e4862e0242026a77babe56776396c2a
naimertiy-max <naimertiy@gmail.com>
2026-06-24 01:04:45 -0300

    Delete Makers directory

--- Makers/Remote Fire.lua
+++
@@ -1,6 +0,0 @@
-local RemotePath = game.ReplicatedStorage.GrabEvents.CreateGrabLine
-if not RemotePath or not RemotePath:IsA("RemoteEvent") then
-    warn("RemoteEvent inválido ou não encontrado")
-    return
-end
-RemotePath:FireAllClients()
